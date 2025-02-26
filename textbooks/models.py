e966739aefbaa4a449866b98548bfdf44976a315
asamerry <asamerry72@gmail.com>
2025-02-25 23:52:32 -0800

    Updated library functionality; added books to a Django model; added dynamic paths; removed excess .html files

--- textbooks/models.py
+++ textbooks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+TYPES = {("book", "Book"), ("paper", "Paper")}
+
+class Keyword(models.Model):
+    name = models.CharField(max_length=99, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Text(models.Model):
+    type = models.CharField(max_length=7, choices=TYPES)
+    title = models.CharField(max_length=99)
+    author = models.CharField(max_length=30)
+    url = models.CharField(max_length=99)
+    keywords = models.ManyToManyField(Keyword)
+
+    def __str__(self):
+        return f"{self.title}; {self.author}"
